d4bc89c3bd06d1128909bf2e654d4bdf0a330060
Nouman-Shahid1 <muhammadnoumansha140@gmail.com>
2024-11-01 15:38:44 +0500

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -12,7 +12,7 @@ export default function Home() {
           height={38}
           priority
         />
-        
+
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
             Get started by editing{" "}
@@ -21,7 +21,6 @@ export default function Home() {
             </code>
             .
           </li>
-          <li>Save and see your changes instantly.</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
